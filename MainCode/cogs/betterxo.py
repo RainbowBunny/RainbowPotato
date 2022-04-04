311248014701a0ce5c37884476f1f872b54ef4cc
rainbowbunny <vmc15566@gmail.com>
2022-04-04 22:11:48 +0700

    Almost finished betterxo.py, need some works on multichannel game

--- MainCode/cogs/betterxo.py
+++ MainCode/cogs/betterxo.py
@@ -1,7 +1,85 @@
 import discord
 import asyncio
 
+from random import randint
+from utils import Utils
 from discord.ext import commands
+
+class Player(discord.Member):
+    pass
+        
+class Board:
+    def __init__(self):
+        self.board = []
+        for i in range(10):
+            self.board.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.char = [1, 2, 4, 3]
+    
+    
+
+    def __repr__(self):
+        def print_num(number):
+            string = str(number)
+            if number < 10:
+                string = ' ' + string
+            return string
+        string = "```\n 0  0 "
+        for i in range(0, 10):
+            string += print_num(i + 1) + " "
+        string += '\n'
+        string += " 0  0"
+        for i in range(10):
+            sum = 0
+            for j in range(10):
+                sum += self.board[j][i]
+            string += " "
+            string += print_num(sum)
+        string += "\n"
+        for i in range(len(self.board)):
+            sum = 0
+            for j in range(10):
+                sum += self.board[i][j]
+            string += print_num(sum)
+            string += ' '
+            string += print_num(i + 1)
+            for j in range(len(self.board[i])):
+                string += ' '
+                string += print_num(self.board[i][j])
+            string += '\n'
+        string += "```"
+        return string
+
+    def valid_move(self, pos):
+        x, y = pos
+        if x < 0 or y < 0 or x >= len(self.board) or y >= len(self.board[0]):
+            return False
+        if self.board[x][y]:
+            return False
+        return True
+
+    def move(self, pos, turn):
+        x, y = pos
+        self.board[x][y] = self.char[turn]
+
+    def status(self):
+        for i in range(10):
+            sumrow, countrow = 0, 0
+            sumcol, countcol = 0, 0
+            for j in range(10):
+                if self.board[i][j]:
+                    sumrow += self.board[i][j]
+                    countrow += 1
+                if self.board[i][j]:
+                    sumcol += self.board[i][j]
+                    countcol += 1
+            if sumrow % 5 == 0 and countrow >= 5:
+                return 1
+            if sumcol % 5 == 0 and countcol >= 5:
+                return 1
+        return 0
+
+
+
 
 class BetterXO(commands.Cog):
     def __init__(self, bot):
@@ -9,71 +87,63 @@ class BetterXO(commands.Cog):
         self.Occupied = []
         self.Inbattle = []
 
+    async def Play(self, ctx, player_list):
+        board = Board()
+        turn = randint(0, 1)
+        if turn:
+            player_list[0], player_list[1] = player_list[1], player_list[0]
+
+        for i in range(2):
+            player_list.append(player_list[i])
+
+        await ctx.send(f'{player_list[0].mention} go first!')
+
+        for i in range(100):
+            curplayer = i % 4
+            await ctx.send(f"It's {player_list[curplayer].mention}\n")
+            await ctx.send(board)
+
+            def check(message):
+                msg = message.content.split()
+                if len(msg) < 2:
+                    return False
+                try:
+                    msg[0], msg[1] = int(msg[0]), int(msg[1])
+                    return board.valid_move([msg[0] - 1, msg[1] - 1])
+                except ValueError:
+                    return False
+
+            message = await self.bot.wait_for('message', check = check)
+            msg = message.content.split()
+            board.move([int(msg[0]) - 1, int(msg[1]) - 1], curplayer)
+
+            if board.status():
+                await ctx.send(f"{player_list[curplayer]} wins!")
+                await ctx.send(board)
+                return       
+
+
+
     @commands.command(name = 'challenge')
     async def challenge(self, ctx, *, name):
-        Founded = 0
-        secondplayer = 0
-        for mem in ctx.guild.members:
-            print(mem.name)
-            if mem.name == name:
-                Founded = 1
-                secondplayer = mem
-                break
+        player_list = []
+        player_list.append(ctx.author)
+        player_list.append(await Utils.find_member(ctx, self.bot, name))
+        if not player_list[1]:
+            await ctx.send('Invalid User!')
         
-        if not Founded:
-            await ctx.send('There is no such user!') 
-            return
-        firstplayer = ctx.author
+        await ctx.send(f'{player_list[0].mention} is challenging {player_list[1].mention}!')
+        def check(message):
+            return message.author.id == player_list[1].id and 'accept' in message.content
         
-        if firstplayer in self.Occupied:
-            await ctx.send('You have already challenging someone!')
-            return
-        
-        if firstplayer in self.Inbattle:
-            await ctx.send('You have already in a match!')
-            return
-
-        if secondplayer in self.Occupied:
-            await ctx.send('This person has already challenging someone!')
-            return
-
-        if secondplayer in self.Inbattle:
-            await ctx.send('This person has already in a match')
-            return
-
-        await ctx.send('A challenger has appeared!')
-        self.Occupied.append(firstplayer)
-        self.Occupied.append(secondplayer)        
-        def CheckMessage(message):
-            if message.author.id == secondplayer.id:
-                string = message.content.lower()
-                return string.startswith('accept') or string.startswith('deny')
-            elif message.author.id == firstplayer.id:
-                string = message.content.lower()
-                return string.startswith('cancel')
-            else:
-                return False
-
         try:
-            Message = await self.bot.wait_for('message', check = CheckMessage, timeout = 60.0)
-            string = Message.content.lower()
-            if string.startswith('deny') or string.startswith('cancel'):
-                await ctx.send('The battle has been canceled.')
-                self.Occupied.remove(firstplayer)
-                self.Occupied.remove(secondplayer)
-                return
-        except asyncio.TimeoutError:
-            await ctx.send('The battle has been canceled.')
-            self.Occupied.remove(firstplayer)
-            self.Occupied.remove(secondplayer)
+            await self.bot.wait_for('message', check = check, timeout = 60)
+        except TimeoutError:
+            await ctx.send('The battle is canceled!')
             return
-        
-        await ctx.send(f'Challenge accepted between {ctx.author.mention} and {user.mention}!')
-        self.Inbattle.append(firstplayer)
-        self.Inbattle.append(secondplayer)
-        self.Occupied.remove(firstplayer)
-        self.Occupied.remove(secondplayer)
-        
+        await ctx.send(f'A battle is starting between {player_list[0].mention} and {player_list[1].mention}!')
+
+        await self.Play(ctx, player_list)
 
 
 def setup(bot):
--- MainCode/utils.py
+++ MainCode/utils.py
@@ -1,5 +1,6 @@
 import json
 from discord.ext import tasks
+import discord
 
 # Init: 
 # data = Reader()
@@ -8,6 +9,55 @@ from discord.ext import tasks
 # data.loop()
 
 # Access file_1.json: data["file_1.json"]
+
+class Utils:
+    @staticmethod
+    async def find_member(ctx, bot, name):
+        if name.startswith("<@") and name.endswith(">"):
+            id = name[2:-1]
+            if id.isdigit(): 
+                return ctx.guild.get_member(int(id))
+        
+        if name.startswith("<@!") and name.endswith(">"):
+            id = name[3:-1]
+            if id.isdigit(): 
+                return ctx.guild.get_member(int(id))
+
+        l = []
+        for i in ctx.guild.members:
+            if i.id == ctx.author.id: continue
+            if i.bot: continue
+            if name.lower() in str(i).lower() or (i.nick and name in i.nick.lower()) or name == str(i.id):
+                l.append(i)
+
+        if len(l) == 0: 
+            await ctx.send("No user found!")
+            return None
+        elif len(l) == 1: return l[0]
+        elif len(l) >= 10: 
+            await ctx.send("Too many members with such name!")
+            return None
+        else: 
+            embed = discord.Embed(title = "Choose a member from below:")
+            d = "\n".join([f"{i + 1} - **{l[i]}**" for i in range(len(l))])
+            embed.description = d
+            await ctx.send(embed = embed)
+            try:
+                msg = await bot.wait_for("message", check = lambda m: m.author == ctx.author and m.channel == ctx.channel, timeout = 30.0)
+                msg = msg.content
+                try:
+                    msg = int(msg)
+                    if msg >= len(l) or msg <= 0: 
+                        await ctx.send("Invalid number!")
+                        return None
+                    else:
+                        return l[msg - 1] 
+                except:
+                    await ctx.send("Invalid number!")
+                    return None
+            except:
+                await ctx.send("Timed out!")
+                return None
 
 class Reader:
     def __init__(self):
